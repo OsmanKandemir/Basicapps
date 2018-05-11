90a23798e4565dea54406b22e69897107b708e01
Osman Kandemir <juanter53@hotmail.com>
2018-05-11 16:42:38 +0300

    Update battery_removed_ inserted.py

--- battery_removed_ inserted.py
+++ battery_removed_ inserted.py
@@ -4,18 +4,29 @@
 
 import psutil
 import winsound
+import time
+
 
 def sound():
-	frekans = 250
-	duration = 100
+	frekans = 1000
+	duration = 1000
 	winsound.Beep(frekans,duration)
-
+h = False
+h2 = False
 while(1):
 	b = psutil.sensors_battery()
-	if b[2]:
-		continue
-	else:
-		print("cikartildi")
-		sound()
 
+	if b[2]:
+                if not h:
+                        print("Sarj Takili Durumda")
+                        h = True
+                h2 = False
+	else:
+                if not h2:
+                        print("Sarjdan Cikarildi")
+                        h2 = True
+                sound()       
+		h = False
+                
+		
 
